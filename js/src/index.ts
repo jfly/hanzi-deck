f625a562fa123697e0d7645a41c49f6267bbebe0
Jeremy Fleischman <jeremyfleischman@gmail.com>
2026-05-04 21:58:18 -0700

    Explicitly render the hanzi write elements
    I haven't really dug into this, but I was seeing many situations (maybe
    every single time?) where the hanzi writer wasn't getting created except
    for the very first card.
    
    I guess if you remove and re-add a `<script src=...>` tag, browsers may
    not actually re-run the javascript if they see they already loaded that
    file? Anyways, I think this works.

--- js/src/index.ts
+++ js/src/index.ts
@@ -1,36 +1,52 @@
 import HanziWriter from "hanzi-writer";
 
-for (let quizEl of document.querySelectorAll<HTMLElement>(".hanzi-quiz")) {
-  let character = quizEl.dataset.character;
-  if (!character) {
-    console.error(
-      "No character specified for .hanzi-quiz element, skipping",
-      quizEl,
-    );
-    continue;
+declare global {
+  interface Window {
+    activateHanziWriters: any;
   }
-
-  let graphicsJson = quizEl.dataset.graphics_json;
-  if (!graphicsJson) {
-    console.error(
-      "No character data present for .hanzi-quiz element, skipping",
-      quizEl,
-    );
-    continue;
-  }
-  let graphics = JSON.parse(graphicsJson);
-
-  let writer = HanziWriter.create(quizEl, character, {
-    width: 300,
-    height: 300,
-    showCharacter: false,
-    showOutline: true,
-    showHintAfterMisses: 1,
-    highlightOnComplete: false,
-    padding: 5,
-    charDataLoader: function () {
-      return graphics;
-    },
-  });
-  writer.quiz();
 }
+
+window.activateHanziWriters = function () {
+  for (let quizEl of document.querySelectorAll<HTMLElement>(".hanzi-quiz")) {
+    let character = quizEl.dataset.character;
+    if (!character) {
+      console.warn(
+        "No character specified for .hanzi-quiz element, skipping",
+        quizEl,
+      );
+      continue;
+    }
+
+    let graphicsJson = quizEl.dataset.graphics_json;
+    if (!graphicsJson) {
+      console.warn(
+        "No character data present for .hanzi-quiz element, skipping",
+        quizEl,
+      );
+      continue;
+    }
+
+    let graphics: any;
+    try {
+      graphics = JSON.parse(graphicsJson);
+    } catch (e) {
+      console.error(e);
+      console.error("Full JSON", graphicsJson);
+      continue;
+    }
+
+    let writer = HanziWriter.create(quizEl, character, {
+      width: 300,
+      height: 300,
+      showCharacter: false,
+      showOutline: true,
+      showHintAfterMisses: 1,
+      highlightOnComplete: false,
+      padding: 5,
+      charDataLoader: function () {
+        return graphics;
+      },
+    });
+    writer.quiz();
+  }
+};
